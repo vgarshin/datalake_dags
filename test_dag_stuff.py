e52ae98befa283348f396dfb9ff76b62fbf97021
vgarshin <vgarshin@yandex.ru>
2022-01-13 13:03:03 +0000

    two tasks

--- test_dag_stuff.py
+++ test_dag_stuff.py
@@ -7,6 +7,11 @@ from airflow.example_dags.libs.helper import print_stuff
 
 from kubernetes.client import models as k8s
 
+from airflow.configuration import conf
+
+worker_container_repository = conf.get('kubernetes', 'worker_container_repository')
+worker_container_tag = conf.get('kubernetes', 'worker_container_tag')
+
 with DAG(
     dag_id="example_pod_template_file",
     schedule_interval=None,
@@ -23,3 +28,29 @@ with DAG(
     @task(executor_config=executor_config_template)
     def task_with_template():
         print_stuff()
+
+    first_task = task_with_template()
+
+    # You can also change the base image, here we used the worker image for demonstration.
+    # Note that the image must have the same configuration as the
+    # worker image. Could be that you want to run this task in a special docker image that has a zip
+    # library built-in. You build the special docker image on top your worker image.
+    kube_exec_config_special = {
+        "pod_override": k8s.V1Pod(
+            spec=k8s.V1PodSpec(
+                containers=[
+                    k8s.V1Container(
+                        name="base", image=f"{worker_container_repository}:{worker_container_tag}"
+                  ),
+                ]
+            )
+        )
+    }
+
+    @task(executor_config=kube_exec_config_special)
+    def base_image_override_task():
+        print_stuff()
+
+    second_task = base_image_override_task()
+
+    first_task >> second_task
