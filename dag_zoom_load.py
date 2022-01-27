6cbb3b54ef165d09f485684e5a8d6dced5a1ebd3
vgarshin <vgarshin@yandex.ru>
2022-01-27 15:13:50 +0000

    zoom data load

--- dag_zoom_load.py
+++ dag_zoom_load.py
@@ -25,10 +25,7 @@ with DAG(dag_id='zoom_data_load',
         volume_mounts=[air_volume_mount, ],
         cmds=[
             "sh", "-c",
-            'cd /home/jovyan/zoomdataload;',
-            'pwd > /home/jovyan/zoomdataload/pwd.txt;',
-            'date > date.txt;',
-            'git clone https://github.com/vgarshin/datalake_scripts',
+            'cd /home/jovyan/zoomdataload; pwd > /home/jovyan/zoomdataload/pwd.txt; date > date.txt; git clone https://github.com/vgarshin/datalake_scripts',
         ],
         startup_timeout_seconds=300,
     )
