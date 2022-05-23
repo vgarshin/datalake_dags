e0f82a242befb11d3669f019fc5d3eb4695c3928
vgarshin <vgarshin@yandex.ru>
2022-05-23 13:52:30 +0000

    all gsom vk loads

--- dag_vk_load.py
+++ dag_vk_load.py
@@ -31,8 +31,8 @@ resource_req = V1ResourceRequirements(
 
 with DAG(dag_id='vk_data_load',
          default_args=default_args,
-         start_date=datetime(2022, 3, 28),
-         end_date=datetime(2027, 3, 28),
+         start_date=datetime(2022, 5, 23),
+         end_date=datetime(2027, 5, 23),
          schedule_interval='0 5 * * 1',
          tags=['vk']) as dag:
     air_volume = V1Volume(
@@ -45,9 +45,9 @@ with DAG(dag_id='vk_data_load',
         mount_path='/home/jovyan/zoomdataload',
         name='airflow-volume'
     )
-    task = KubernetesPodOperator(
-        task_id='vk_data_load_run_script',
-        name='vk_load_run_script',
+    task1 = KubernetesPodOperator(
+        task_id='vk_data_load_run_script_ma',
+        name='vk_load_run_script_ma',
         namespace='airflow',
         image='vgarshin/mibapysparks3:20220204v0',
         resources=resource_req,
@@ -56,6 +56,36 @@ with DAG(dag_id='vk_data_load',
         cmds=[
             "sh", "-c",
             'git clone https://github.com/vgarshin/datalake_scripts && python datalake_scripts/vk_load.py gsom_ma'
+        ],
+        is_delete_operator_pod=True,
+        startup_timeout_seconds=900,
+    )
+    task2 = KubernetesPodOperator(
+        task_id='vk_data_load_run_script_bak',
+        name='vk_load_run_script_bak',
+        namespace='airflow',
+        image='vgarshin/mibapysparks3:20220204v0',
+        resources=resource_req,
+        volumes=[air_volume, ],
+        volume_mounts=[air_volume_mount, ],
+        cmds=[
+            "sh", "-c",
+            'git clone https://github.com/vgarshin/datalake_scripts && python datalake_scripts/vk_load.py gsom_abiturient'
+        ],
+        is_delete_operator_pod=True,
+        startup_timeout_seconds=900,
+    )
+    task3 = KubernetesPodOperator(
+        task_id='vk_data_load_run_script_spbu',
+        name='vk_load_run_script_spbu',
+        namespace='airflow',
+        image='vgarshin/mibapysparks3:20220204v0',
+        resources=resource_req,
+        volumes=[air_volume, ],
+        volume_mounts=[air_volume_mount, ],
+        cmds=[
+            "sh", "-c",
+            'git clone https://github.com/vgarshin/datalake_scripts && python datalake_scripts/vk_load.py gsom.spbu'
         ],
         is_delete_operator_pod=True,
         startup_timeout_seconds=900,
